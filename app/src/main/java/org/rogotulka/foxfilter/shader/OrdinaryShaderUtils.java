18b40eee1beb06c827f8c65e59a5e5cf400ff73f
Ann Rozhkova <a.rozhkova@nemotelecom.co.uk>
2015-07-29 20:30:45 +0300

    add ordinary shader

---
+++ app/src/main/java/org/rogotulka/foxfilter/shader/OrdinaryShaderUtils.java
@@ -0,0 +1,30 @@
+package org.rogotulka.foxfilter.shader;
+
+/**
+ *
+ */
+public class OrdinaryShaderUtils {
+
+    protected final static String mVertexShader =
+                    "uniform mat4 uMVPMatrix;" +
+                    "attribute vec4 vPosition;" +
+                    "attribute vec4 a_Color;" +
+                    "attribute vec2 a_texCoord;" +
+                    "varying vec4 v_Color;" +
+                    "varying vec2 v_texCoord;" +
+                    "void main() {" +
+                    "  gl_Position = uMVPMatrix * vPosition;" +
+                    "  v_texCoord = a_texCoord;" +
+                    "  v_Color = a_Color;" +
+                    "}";
+
+    protected final static String mFragmentShader =
+                    "precision mediump float;" +
+                    "varying vec2 v_texCoord;" +
+                    "varying vec4 v_Color;" +
+                    "uniform sampler2D s_texture;" +
+                    "void main() {" +
+                    "  gl_FragColor = texture2D( s_texture, v_texCoord ) * v_Color;" +
+                    "  gl_FragColor.rgb *= v_Color.a;" +
+                    "}";
+}
