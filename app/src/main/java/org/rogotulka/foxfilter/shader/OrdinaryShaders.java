59d2c2337b6878f464ae91cbc02cf685ef5c44db
rogotulka <rogotulka@gmail.com>
2015-07-31 10:35:00 +0300

    fix OrdinaryShaders

--- app/src/main/java/org/rogotulka/foxfilter/shader/OrdinaryShaders.java
+++ app/src/main/java/org/rogotulka/foxfilter/shader/OrdinaryShaders.java
@@ -5,7 +5,7 @@ package org.rogotulka.foxfilter.shader;
  */
 public class OrdinaryShaders implements Shader{
 
-    protected final static String mVertexShader =
+    protected static String mVertexShader =
                     "uniform mat4 uMVPMatrix;" +
                     "attribute vec4 vPosition;" +
                     "attribute vec4 a_Color;" +
@@ -18,7 +18,7 @@ public class OrdinaryShaders implements Shader{
                     "  v_Color = a_Color;" +
                     "}";
 
-    protected final static String mFragmentShader =
+    protected static String mFragmentShader =
                     "precision mediump float;" +
                     "varying vec2 v_texCoord;" +
                     "varying vec4 v_Color;" +
@@ -27,4 +27,14 @@ public class OrdinaryShaders implements Shader{
                     "  gl_FragColor = texture2D( s_texture, v_texCoord ) * v_Color;" +
                     "  gl_FragColor.rgb *= v_Color.a;" +
                     "}";
+
+    @Override
+    public String getVertexShader() {
+        return mVertexShader;
+    }
+
+    @Override
+    public String getFragmentShader() {
+        return mFragmentShader;
+    }
 }
