0070bf31e2df27b2cbb05bd278670cef652de6d9
rogotulka <rogotulka@gmail.com>
2015-09-11 01:25:16 +0300

    add future save class

---
+++ app/src/main/java/org/rogotulka/foxfilter/utils/ImageStorable.java
@@ -0,0 +1,8 @@
+package org.rogotulka.foxfilter.utils;
+
+import android.graphics.Bitmap;
+
+
+public interface ImageStorable {
+    void save(Bitmap bitmap);
+}
---
+++ app/src/main/java/org/rogotulka/foxfilter/utils/SDCardImageStorableImpl.java
@@ -0,0 +1,11 @@
+package org.rogotulka.foxfilter.utils;
+
+import android.graphics.Bitmap;
+
+public class SDCardImageStorableImpl implements ImageStorable {
+
+    @Override
+    public void save(Bitmap bitmap) {
+
+    }
+}
